d0983ac5dc3099ce47cd862cb7b47d671ccd2321
chlor <chlor>
2000-12-11 00:58:46 +0000

    Link rettet fra prog til program

--- program/eksempler/show.php
+++ program/eksempler/show.php
@@ -9,7 +9,7 @@
 	echo "<h1>".$proglang[$ext][navn]."</h1>\n";
 	echo "<h2>Filnavn: $filename</h2>\n";
 	echo "Læs mere om <b>".$proglang[$ext][navn]."</b> i bogen under ";
-	echo "<a href=\"$boghref".$proglang[$ext][link]."\">http://www.sslug.dk$boghref".$proglang[$ext][link]."</a>\n";
+	echo "<a href=\"$boghref".$proglang[$ext][link]."\">http://$SERVER_NAME$boghref".$proglang[$ext][link]."</a>\n";
 	echo "<p>\n";
 	echo $examp[$file];
   echo "<p>Download <a href=\"$filename\">$filename</a>\n";
