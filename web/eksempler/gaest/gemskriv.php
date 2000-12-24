b3c1ec28cee44975e575ab9fdda71418306fe3d6
chlor <chlor>
2000-12-24 14:57:32 +0000

    Simple web-database eksempler. En gæstebog.

---
+++ web/eksempler/gaest/gemskriv.php
@@ -0,0 +1,31 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
+        "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+<head>
+<title>Min gæstebog</title>
+</head>
+<body bgcolor="white">
+
+<?php
+
+function gem_denne($navn, $email, $hilsen ) {
+  // Hent password
+  require(".password.inc");
+  // Åben forbindelse til databasen
+  $conn = pg_pconnect("dbname=$dbname user=$dbuser");
+
+  $sql = "INSERT INTO gaest(navn,email,hilsen) VALUES('$navn', '$email', '$hilsen')";
+  $res = pg_exec($conn, $sql );
+  pg_freeresult($res);
+
+  pg_close($conn);
+}
+
+// Test om der blev klikket 'Gem'
+if ($aktion == "Gem") {
+  echo "Hej <b>$navn</b><br>Tak fordi du skrev i min gæstebog.";
+  gem_denne($navn, $email, $hilsen);
+}
+
+?>
+</body></html>
---
+++ web/eksempler/gaest/visalle.php
@@ -0,0 +1,29 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN"
+        "http://www.w3.org/TR/REC-html40/loose.dtd">
+<html>
+<head>
+<title>Min gæstebog</title>
+</head>
+<body bgcolor="white">
+
+<?php
+
+  // Hent password
+  require(".password.inc");
+  // Åben forbindelse til databasen
+  $conn = pg_pconnect("dbname=$dbname user=$dbuser");
+
+  $sql = "SELECT opdat,navn,email,hilsen FROM gaest ORDER BY opdat DESC";
+  $res = pg_exec($conn, $sql );
+  for ($n=0; $n <pg_numrows($res); $n++ ) {
+    list($opdat,$navn,$email,$hilsen) = pg_fetch_row($res, $n);
+    echo "<h1>$navn &lt;email&gt;</h1>";
+    echo "<i>$opdat</i><br>";
+    echo $hilsen;
+  }
+  pg_freeresult($res);
+
+  pg_close($conn);
+
+?>
+</body></html>
