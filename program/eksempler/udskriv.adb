c5581f9afac98ea327a9c0fccf841edd9484134f
sparre <sparre>
2001-01-09 12:11:25 +0000

    Skrevet Ada-udgaven af "udskriv" om, så det udnytter flere indbyggede routiner og følger stilvejledningen (lidt mere).

--- program/eksempler/udskriv.adb
+++ program/eksempler/udskriv.adb
@@ -1,60 +1,99 @@
--- af Peter Stubbe <stubbe@bitnisse.dk>
--- $Id$
+--  af Peter Stubbe <stubbe@bitnisse.dk>
+--  $Id$
 
--- Programmet er skrevet til Gnat
--- Oversættelse:
---  gnatmake udskriv.adb
+--  2000.01.09: Skrevet om/rettet til af Jacob Sparre Andersen <sparre@nbi.dk>
+
+--  Programmet er afprøvet med GNAT (GNU Ada), men bør virke med alle
+--  Ada-oversættere.
 --
--- Afvikling:
---  ./udskriv [fil]+
+--  Oversættelse:
+--    gnatmake udskriv -cargs -gnatv -gnati1 -gnatf -gnato
+--
+--  Afvikling:
+--    ./udskriv [fil]+
 
-with Ada.Text_IO; use Ada.Text_IO;
+with Ada.Characters.Handling;
 with Ada.Command_Line;
+with Ada.Strings.Fixed;
+with Ada.Text_IO;
 
 procedure Udskriv is
-package IntegerIO is new Ada.Text_IO.Integer_Io(Integer);
-use IntegerIO;
 
-  function Findesfil(Navn: String) return Boolean is
-  Fil: Ada.Text_Io.File_Type;
-  begin
-    Open(Fil, In_File, Navn);
-    Close(Fil);
-    return True;
+   ---------------------------------------------------------------------------
 
-  exception
-    when Name_Error => return False;
-  end;
+   package Count_Text_IO is new Ada.Text_IO.Integer_IO (Ada.Text_IO.Count);
 
-Lnr: Integer;
-Fil: Ada.Text_Io.File_Type;
-I: Integer;
-C:Character;
-begin
-  for I in 1 .. Ada.Command_Line.Argument_Count loop
-    Lnr:=1;
-    if not Findesfil(Ada.Command_Line.Argument(I)) then
-      Put("Filen ");
-      Put(Ada.Command_Line.Argument(I));
-      Put_Line(" findes ikke!");
-    else
-      Open(Fil,In_file,Ada.Command_Line.Argument(I));
-      while not End_Of_File(Fil) loop
-        Put(Lnr, 1);
-        Set_Col(10);
-        while not End_Of_Line(Fil) loop
-          Get(Fil, c);
-          Put(c);
-        end loop;
-        Put_Line("");
-        Skip_Line(Fil);
-        Lnr:=Lnr+1;
+   ---------------------------------------------------------------------------
+
+   procedure Vent_På (Tegn : in     String) is
+
+      use Ada.Strings.Fixed;
+      use Ada.Text_IO;
+
+      Tast    : Character;
+      Trykket : Boolean;
+
+   begin
+      New_Line;
+      loop
+         Get_Immediate (Item      => Tast,
+                        Available => Trykket);
+         exit when Index (Source  => Tegn,
+                          Pattern => (1 => Tast)) > 0;
       end loop;
-      Close(Fil);
-    end if;
-  end loop;
-  C:=' ';
-  while C /= 'Q' loop
-    Get(C);
-  end loop;
-end;
+      New_Line;
+   end Vent_På;
+
+   ---------------------------------------------------------------------------
+
+   use Ada.Command_Line;
+
+begin
+   for Indeks in 1 .. Argument_Count loop
+
+   Udskriv_Fil:
+      declare
+         use Ada.Characters.Handling;
+         use Ada.Text_IO;
+         use Count_Text_IO;
+
+         Navn  : String renames Argument (Indeks);
+         Fil   : File_Type;
+         Tegn  : Character;
+      begin
+         Open (File => Fil,
+               Name => Navn,
+               Mode => In_File);
+
+      Læs_Linier:
+         while not End_Of_File (Fil) loop
+            Put (Item  => Line (Fil),
+                 Width => 5);
+            Set_Col (10);
+
+            while not End_Of_Line (Fil) loop
+               Get (File => Fil,
+                    Item => Tegn);
+
+               if Is_Graphic (Tegn) then
+                  Put (Item => Tegn);
+               else
+                  Put (Item => "¿");
+               end if;
+
+               exit Læs_Linier when End_Of_File (Fil);
+            end loop;
+
+            Skip_Line (File => Fil);
+            New_Line;
+         end loop Læs_Linier;
+
+         Close (File => Fil);
+
+         Vent_På ("QqNn");
+      exception
+         when Name_Error =>
+            Put_Line ("Filen " & Navn & " findes ikke!");
+      end Udskriv_Fil;
+   end loop;
+end Udskriv;
