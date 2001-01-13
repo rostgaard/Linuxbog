07c9151c83e4f1446dba152ecf706e46a218adc9
chlor <chlor>
2001-01-13 16:22:47 +0000

    Script til at splitte http og ftp URLer. PostScript og PDF problem

---
+++ httpsplit.pl
@@ -0,0 +1,29 @@
+#!/usr/bin/perl -w -i -p
+ 
+# Script af Hans Schou (og Ole Tange har også hjulpet lidt med et par "småting").
+# $Id$
+
+# Afvikling:
+#  ./httpsplit *.sgml
+
+# Hyperlinks i PostScript og PDF giver så lange tekster at
+# links går ud over kanten af papiret, og kan så ikke læses.
+# Scriptet her fjerner <ULINK> og sætter spacer før '/' i URLer.
+
+# Filerne bliver ændret, så dette script må kun køres på en kopi.
+
+undef ($/);
+
+ # Lav mailto om
+ s!<ulink\s+url="mailto:(.*?)">.*?</ulink>!<ComMand>&lt;$1&gt;</ComMand>!gsi;
+ 
+ # Lav http og ftp URLer om
+ s!<ulink\s+url="     # <ulink url="
+    (.tt?p://|file:)  # http://, ftp:// eller file:
+    (.*?)">           # www.sslug.dk/linuxbog">
+    .*?</ulink>       # www.sslug.dk/linuxbog</ulink>
+  !
+    $http=$1;$path=$2;$path =~ s@/@ /@g;
+    "<FileName>$http$path</FileName>"
+  !gsixe;             # Global SingleString IgnoreCase eXtentedComments Expression
+
