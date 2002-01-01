5c08ec140972817e06143854d9ead1036c4d0694
pto <pto>
2002-01-01 19:57:30 +0000

    Opdatering kvi

--- admin/eksempler/set_domain_ip.sh
+++ admin/eksempler/set_domain_ip.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
-
+#
+# chkconfig: 35 11 99
+# description: set FQDN and modify /etc/hosts in braindead DHCP environments
+# processname: N/A
+# config: N/A
+# pidfile: N/A
 ############################################################################
 ####################
 ### 30/12-2001 Michael Rasmussen.
@@ -12,22 +17,32 @@
 ### Revision history
 ###
 ### Version 0.1:   Initial script
+###	Version 0.2:   Applied support for chkconfig by Kristian Vilmann
 ###
 ### Installation (Redhat 7.x)
 ###
 ### 1) Log in as root (su -)
-### 2) Copy the script to /etc/rc.d/init.d
-### 3) Chmod 700 /etc/rc.d/init.d/set_domain_ip.sh
-### 4) Open the file /etc/rc.d/rc.local in your favorit editor
-### 5) Write at the bottom: /etc/rc.d/init.d/set_domain_ip.sh
+### 2) Copy the script to /etc/rc.d/init.d/set_domain_ip
+### 3) Chmod 700 /etc/rc.d/init.d/set_domain_ip
+### 4) Run /sbin/chkconfig --add set_domain_ip
 ###    
 ############################################################################
 ####################
 
+case "$1" in
+  start)
+	# Fall through
+	;;
+  stop)
+	# Exit Now
+	exit 0
+	;;
+esac
+	 
 dev=eth0
 hosts=/etc/hosts
 
-eth0=`ifconfig $dev|grep inet|awk -F: '{print $2}'|awk '{print $1}'`
+eth0=`/sbin/ifconfig $dev|grep inet|awk -F: '{print $2}'|awk '{print $1}'`
 
 nickname=`echo $HOSTNAME|awk -F. '{print $1}'`
 
