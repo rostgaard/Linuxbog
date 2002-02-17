fb60137258229e727b3998a16e82b64a466b16a5
pto <pto>
2002-02-17 22:18:42 +0000

    Vis kun top og bund menu hvis de findes

--- index.php
+++ index.php
@@ -13,7 +13,9 @@
    $bodyarg=" background=\"/grafix/linux-back-1.gif\" ";
    $maintain_name = "Hans Schou";       // Skriv dit navn her
    $maintain_email = "chlor@sslug.dk";  // Skriv din email adresse her
-   include($DOCUMENT_ROOT."includes/top.phtml");
+   if (file_exists($DOCUMENT_ROOT."includes/top.phtml")) {
+     include($DOCUMENT_ROOT."includes/top.phtml");
+   }
 
    list($width,$height) = getimagesize("front.png");
 
@@ -522,5 +524,7 @@ Vil du hjælpe med, så læs <a href="hjaelpe.html">mere her</a>.
 <!-- Text slut -->
 <!-- Husk din email-adresse: -->
 <?php
-  include($DOCUMENT_ROOT."includes/bottom.phtml");
+  if (file_exists($DOCUMENT_ROOT."includes/bottom.phtml")) {
+    include($DOCUMENT_ROOT."includes/bottom.phtml");
+  }
 ?>
