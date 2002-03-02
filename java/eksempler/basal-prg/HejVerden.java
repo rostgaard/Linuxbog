be0fa95531ee33963ed7e00773f188fa959a0d9d
jonas <jonas>
2002-03-02 12:51:58 +0000

    Tilføjet eksemplet HejVerden.java

---
+++ java/eksempler/basal-prg/HejVerden.java
@@ -0,0 +1,11 @@
+// Et simpelt program, der skriver "Hej verden" 
+// og et citat af Storm P. ud til skærmen
+public class HejVerden
+{
+  public static void main (String[] args)
+  {
+    System.out.println("Hej Verden!");
+    System.out.println("Hvornår smager en Tuborg bedst?");
+    System.out.println("Hvergang!");
+  }
+}
