d214f574f0293f227612ac453f069d0b8b16705c
madsdyd <madsdyd>
2003-10-06 20:16:44 +0000

    Forsøgt at fikse Hans splitindexscript. Kig efter filer kaldet idx.html

--- alle/fix_stikord.html.pl
+++ alle/fix_stikord.html.pl
@@ -14,5 +14,11 @@ while(<>) {
     s/\<H2 /\n\n\<H2 /g;
     s/\<DL/\n\<DL/g;
     s/ \>/\>/g;
+    # Splitte dt'er, så de kan tælles.
+    s/\<DT\>/\n\<DT\>/g;
+    # Fjerne divs som er noget snavs alligevel.
+    s/\<\/DIV\>//g;
+    s/\<DIV CLASS=\"indexdiv\"\>//g;
+    s/\<DIV CLASS=\"NAVFOOTER\"\>.*//g;
     print $_;
 }
--- alle/splitindex.pl
+++ alle/splitindex.pl
@@ -20,6 +20,10 @@ my $header2 = "</TITLE>
 
 open(FILE,">/dev/null");
 open(IDX,">idx.html") || die "Can not write to idx.html";
+# Det her tælle virker ikke..
+$lastcountentry = 0;
+$lastfile = "";
+$lastfiletitle = "";
 while (<>) {
     # Kæmpestart
     if ($state == 0) {
@@ -34,25 +38,45 @@ while (<>) {
     }
     # Åbning af fil?
     if ($state == 1) {
+	# Matcher nyt "bogstav".
 	if (m/<H2 CLASS="indexdiv"><A NAME="[a-zA-Z0-9]+"><\/A>([a-zA-Z]+)<\/H2>/) {
 #	    print STDERR "2: Match på : $_";
+
+	    # Regn ud hvad vi nu beskæftiger os med.
 	    $filetitle = $1;
 	    $file = "idx-".lc $filetitle.".html";
-	    print IDX "<li><a href=\"$file\">$filetitle</a></li>\n";
+
+	    # Fiks idx.html filen.
+	    if ("" ne $lastfile) {
+		print IDX "<li><a href=\"$lastfile\">$lastfiletitle</a> ($lastcountentry)</li>\n";
+	    }
+	    $lastfile = $file;
+	    $lastfiletitle = $filetitle;
+	    $lastcountentry = 0;
+	    
+	    # Det her virker tilsyneladende, selvom FILE ikke er sat.
 	    print FILE "</body></html>";
 	    close(FILE);
+
+	    # Åben den næste.
 	    open(FILE,">$file") || die "Can not write to $file";
 	    print FILE $header1.$filetitle.$header2;
 	    print FILE "<h1>$filetitle</h1>\n";
 	} else {
+	    if (m/\<DT\>/) { $lastcountentry++; }
 #	    print STDERR "2: Ej Match på : $_";
-	    if (
 	    print FILE $_;
 	}
 
     }
 }
+
+
+if ("" ne $lastfile) {
+    print IDX "<li><a href=\"$lastfile\">$lastfiletitle</a> ($lastcountentry)</li>\n";
+}
 print IDX "</ul></body></html>";
 close(IDX);
+
 print FILE "</body></html>";
 close(FILE);
