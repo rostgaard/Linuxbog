49431a7e6f868857a3efd44608296ab50b1b10a8
chlor <chlor>
2003-10-31 23:08:28 +0000

    Måske er dette et pænere output.

--- alle/splitindex.pl
+++ alle/splitindex.pl
@@ -16,14 +16,18 @@ my $header1 = "<!DOCTYPE html PUBLIC \"-//W3C//DTD HTML 4.01 Transitional//EN\">
 my $header2 = "</TITLE>
     <META name=\"GENERATOR\" content=\"splitindex.pl\">
     <LINK rel=\"STYLESHEET\" type=\"text/css\" href=\"linuxbog.css\">
+    <META HTTP-EQUIV=\"Content-Type\" CONTENT=\"text/html; charset=ISO-8859-1\">
   </HEAD>
 
   <BODY class=\"SETINDEX\">
+  <H1>Stikordsregister - Friheden Til At Vælge - Alle bøger</H1>
+  <p>[<a href=\"idx.html\">Bogstaver</a>] [<a href=\"stikord.html\">Samlet liste af stikord</a>] [<a href=\"index.html\">Indhold</a>]</p>
 ";
 
 open(FILE,">/dev/null");
 open(IDX,">idx.html") || die "Can not write to idx.html";
 # Det her tælle virker ikke..
+$totalcountentry = 0;
 $lastcountentry = 0;
 $lastfile = "";
 $lastfiletitle = "";
@@ -33,7 +37,7 @@ while (<>) {
 	if (m/indexdiv/) {
 #	    print STDERR "1: Match på : $_";
 	    $state = 1;
-	    print IDX $header1."Oversigt".$header2."\n<ul>\n";
+	    print IDX $header1."Oversigt".$header2."\n<p>\n";
 	} else {
 #	    print STDERR "1: Ej Match på : $_";
 	    $headerX .= $_;
@@ -42,7 +46,7 @@ while (<>) {
     # Åbning af fil?
     if ($state == 1) {
 	# Matcher nyt "bogstav".
-	if (m/<H2 CLASS="indexdiv"><A NAME="[a-zA-Z0-9]+"><\/A>([a-zA-Z]+)<\/H2>/) {
+	if (m/<H2 CLASS="indexdiv"><A NAME="[a-zA-Z0-9æÆ]+"><\/A>([a-zA-Z]+)<\/H2>/i) {
 #	    print STDERR "2: Match på : $_";
 
 	    # Regn ud hvad vi nu beskæftiger os med.
@@ -51,10 +55,11 @@ while (<>) {
 
 	    # Fiks idx.html filen.
 	    if ("" ne $lastfile) {
-		print IDX "<li><a href=\"$lastfile\">$lastfiletitle</a> ($lastcountentry)</li>\n";
+		print_a_letter($lastfile,$lastfiletitle,$lastcountentry);
 	    }
 	    $lastfile = $file;
 	    $lastfiletitle = $filetitle;
+            $totalcountentry += $lastcountentry;
 	    $lastcountentry = 0;
 	    
 	    # Det her virker tilsyneladende, selvom FILE ikke er sat.
@@ -76,10 +81,21 @@ while (<>) {
 
 
 if ("" ne $lastfile) {
-    print IDX "<li><a href=\"$lastfile\">$lastfiletitle</a> ($lastcountentry)</li>\n";
+    print_a_letter($lastfile,$lastfiletitle,$lastcountentry);
 }
-print IDX "</ul></body></html>";
+print IDX "</p>\n";
+print IDX "<p>Klik på et bogstav for at se listen med ord der begynder med dette.</p>\n";
+print IDX "<p>Samlet er der $totalcountentry stikord.</p>\n";
+print IDX "</body></html>";
 close(IDX);
 
 print FILE "</body></html>";
 close(FILE);
+
+sub print_a_letter {
+	my $lastfile = shift;
+	my $lastfiletitle = shift;
+	my $lastcountentry = shift; # ...men antal bruger vi ikke
+	print IDX "[<a href=\"$lastfile\">$lastfiletitle</a><!-- $lastcountentry -->]\n";
+}
+
