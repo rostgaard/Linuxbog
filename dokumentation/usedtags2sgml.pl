04505a666397a5f3e9e40818d2894d15beab4f4e
chlor <chlor>
2006-02-24 00:12:49 +0000

    Appendix "Elementer anvendt i denne bog" vises nu i en tabel hvor der er link til bogen selv, docbook.org og oreilly.com .
    Ny fil: usedtags2sgml.pl
    Programmet søger efter "docbook-<navn>" i docbook.sgml og laver så
    et link til det id. Nogle id'er har jeg skrevet om, da de havde
    forkerte navne (så jeg ikke kunne bruge dem).
    Jeg håber ikke id'er blev brugt eksternt i anden sammenhæng.

--- dokumentation/usedtags2sgml.pl
+++ dokumentation/usedtags2sgml.pl
@@ -7,12 +7,12 @@
 
 print "<table id=\"anvendte-elementer-tabel\">
 <title>Anvendte elementer i denne bog</title>
-<tgroup cols=4 align=\"char\">
+<tgroup cols=\"4\" align=\"char\">
 <thead><row>
 <entry>Antal</entry>
 <entry>Navn</entry>
-<entry>O'reilly</entry>
 <entry>DocBook.org</entry>
+<entry>O'reilly</entry>
 </row></thead><tbody>\n";
 
 while (<>) {
@@ -22,15 +22,15 @@ while (<>) {
 		$name = $2;
 		print "<row>";
 		printf("<entry>%4d</entry>", $count);
-		$found = `egrep -i 'sect3 *id=\"docbook-$name\"' docbook.sgml`;
-		#print "\n**** $found\n"; exit;
-		if ($found) {
+		$found = `egrep -i 'sect[1-3] *id=\"docbook-$name\"' docbook.sgml`;
+		if ((length $found) eq 0) {
 			print "<entry>$name</entry>";
 		} else {
+			print "\n<!-- '$found' -->\n";
 			print "<entry><link linkend=\"docbook-$name\">$name</link></entry>";
 		}
-		print "<entry><ulink url=\"http://www.docbook.org/tdg/en/html/$name.html\">docbook.org</ulink></entry>";
-		print "<entry><ulink url=\"http://www.oreilly.com/catalog/docbook/chapter/book/$name.html\">O'reilly</ulink></entry> ";
+		print "<entry><ulink url=\"http://www.docbook.org/tdg/en/html/$name.html\">docbook.org $name</ulink></entry>";
+		print "<entry><ulink url=\"http://www.oreilly.com/catalog/docbook/chapter/book/$name.html\">O'reilly $name</ulink></entry> ";
 		print "</row>\n";
 	}
 }
